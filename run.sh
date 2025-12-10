40de4bd7d61944f868a039391d2d18a9666d83d6
xm0625 <402276694@qq.com>
2025-12-10 21:03:41 +0800

    add Crontab

--- run.sh
+++ run.sh
@@ -4,5 +4,5 @@
 cp /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
 echo Asia/Shanghai > /etc/timezone
 
-/bin/bash /CronRootSync.sh;
+/bin/sh /CronRootSync.sh;
 /usr/sbin/crond -n -x proc 2>&1 | stdbuf -o0 grep '^log_it' | stdbuf -o0 grep -Ev "CronRootSync.sh" | stdbuf -o0 grep -Ev "FileNoChange"
